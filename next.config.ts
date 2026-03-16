bc1c78006231dc2f631bc34cbeedba2f0bfd7832
tummybutters <your_github_email@example.com>
2026-03-16 10:11:53 -0700

    big rollback

--- next.config.ts
+++ next.config.ts
@@ -3,36 +3,6 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
-  async rewrites() {
-    return [
-      {
-        source: "/sitesdone-landing",
-        destination: "/sitesdone-landing/index.html",
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: "/sitesdone-landing",
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "noindex, nofollow",
-          },
-        ],
-      },
-      {
-        source: "/sitesdone-landing/:path*",
-        headers: [
-          {
-            key: "X-Robots-Tag",
-            value: "noindex, nofollow",
-          },
-        ],
-      },
-    ];
-  },
   async redirects() {
     return [
       {
